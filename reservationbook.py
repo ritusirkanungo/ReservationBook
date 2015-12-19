6007ca4b7ce74d14a2bce64e9fe91c8f9f13ce90
Ritu Sirkanungo <ritusirkanungo@gmail.com>
2015-12-19 05:24:42 -0500

    rss added, image upload, validation check, changes after testing

--- reservationbook.py
+++ reservationbook.py
@@ -4,9 +4,16 @@ import os
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
+from google.appengine.api import mail
+from google.appengine.api import images
 from datetime import datetime,time,date,timedelta
 from time import sleep
+from xml.etree.ElementTree import Element, SubElement, Comment
+from xml.etree import ElementTree
+from xml.dom import minidom
+from email import utils
 
+import time
 import webapp2
 import jinja2
 import uuid
@@ -23,6 +30,15 @@ def reservationbook_key(reservationbook_name=DEFAULT_RESERVATIONBOOK_NAME):
     """
     return ndb.Key('AddCreatedResource', reservationbook_name)
 
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ElementTree.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
+
+	
+	
 class Author(ndb.Model):
 	"""Sub model to represent an author"""
 	identity=ndb.StringProperty(indexed=False)
@@ -37,6 +53,8 @@ class Resource(ndb.Model):
 	author=ndb.StructuredProperty(Author)
 	date=ndb.DateTimeProperty(auto_now_add=False)
 	tags=ndb.StringProperty(repeated=True)
+	pubDate=ndb.DateTimeProperty(auto_now_add=True)
+	avatar = ndb.BlobProperty()
 	
 class Reservation(ndb.Model):
 	author=ndb.StructuredProperty(Author)
@@ -46,12 +64,14 @@ class Reservation(ndb.Model):
 	startTime = ndb.DateTimeProperty(auto_now_add=False)
 	endTime = ndb.DateTimeProperty(auto_now_add=False)	
 	duration = ndb.StringProperty(indexed=False)
+	pubDate = ndb.DateTimeProperty(auto_now_add=True)
 	
 	
 class MainPage(webapp2.RequestHandler):
 	def get(self):
 		resource_query=Resource.query().order(-Resource.date)
 		resources=resource_query.fetch()
+		
 		
 		reservation_query=Reservation.query()
 		reservations = reservation_query.fetch()
@@ -172,7 +192,9 @@ class AddCreatedResource(webapp2.RequestHandler):
 		
 		addResource.uuid = str(uuid.uuid4())
 		
+		avatar = self.request.get('img')
 		
+		addResource.avatar = str(avatar)
 		
 		addResource.put()
 		sleep(2)
@@ -226,6 +248,7 @@ class AddReservation(webapp2.RequestHandler):
 		resource = resource_query.fetch()
 		resource = resource[0]
 		resource.date= datetime.now()
+		
 		resource.put()
 		sleep(1)
 		
@@ -280,6 +303,9 @@ class AddReservation(webapp2.RequestHandler):
 			addReservation.uuid = str(uuid.uuid4())
 			
 			addReservation.put()
+			
+			
+			
 			sleep(2)
 			query_params={'menu_name':menu_name}
 			self.redirect('/?'+urllib.urlencode(query_params))		
@@ -309,11 +335,109 @@ class ListTagResources(webapp2.RequestHandler):
 
 		else:
 			self.redirect(users.create_login_url(self.request.uri))
-	
+
+class RSSResource(webapp2.RequestHandler):
+	def get(self):
+		resource_uuid = self.request.get('get_ResourceID')
+		
+		reservation_query = Reservation.query(Reservation.resourceUUID == resource_uuid)
+		reservations = reservation_query.fetch()
+		user = users.get_current_user()
+		resource_query = Resource.query(Resource.uuid == resource_uuid)
+		resource = resource_query.fetch()
+		weburl = self.request.url
+		urlstringsplit = weburl.split("/")
+		urlstringsplit.remove(urlstringsplit[len(urlstringsplit)-1])
+		strjoin = "/".join(urlstringsplit)
+		
+		strjoin = strjoin + "/reserveResource?resource_uuid="+resource_uuid
+		
+		
+		
+		top = Element('rss')
+		top.set('version', '2.0')
+
+		channel = SubElement(top, 'channel')
+		title = SubElement(channel, 'title')
+		title.text = resource[0].resourceName
+		description = SubElement(channel, 'description')
+		description.text = "This resource is owned by "+ resource[0].author.email
+		link = SubElement(channel, 'link')
+		link.text = strjoin
+		lastBuildDate = SubElement(channel, 'lastBuildDate')
+		
+		resourcepubdate = resource[0].pubDate.timetuple()
+		pubtimestamp = time.mktime(resourcepubdate)
+
+		
+
+		if resource[0].date is None:
+			lastBuildDate.text = utils.formatdate(pubtimestamp)
+		else:
+			resourcelastdate = resource[0].date.timetuple()
+			lasttimestamp = time.mktime(resourcelastdate)
+			lastBuildDate.text = utils.formatdate(lasttimestamp)
+		
+		pubDate = SubElement(channel, 'pubDate')
+		pubDate.text = utils.formatdate(pubtimestamp)
+		count = 1
+		if len(reservations) > 0:
+			for reservation in reservations:
+				reservationpubdate = reservation.pubDate.timetuple()
+				reservepubtimestamp = time.mktime(reservationpubdate)
+
+				item = SubElement(channel, 'item')
+				title = SubElement(item, 'title')
+				title.text = "Reservation"+str(count)
+				description = SubElement(item, 'description')
+				description.text = "This reservation is done by:"+reservation.author.email+"with start time:"+str(reservation.startTime)+" for duration:"+ str(reservation.duration)
+				link = SubElement(item, 'link')
+				link.text = strjoin
+				guid = SubElement(item, 'guid')
+				guid.set('isPermaLink','false')
+				guid.text = reservation.uuid
+				pubDate = SubElement(item, 'pubDate')
+				pubDate.text = utils.formatdate(reservepubtimestamp)
+
+				count = count + 1
+		
+		xmlFile = prettify(top)
+		
+		if user:
+			url=users.create_logout_url(self.request.uri)
+			url_linktext='Logout'
+			
+			template_values = {
+				'reservations':reservations,
+				'user':user,
+				'xmlFile':xmlFile,
+			}
+			template = JINJA_ENVIRONMENT.get_template('seeRSS.html')
+			self.response.write(template.render(template_values))
+
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
+
+class Image:
+	def get(self):
+		resource_key = ndb.Key(urlsafe=self.request.get('img_id'))
+		resource = resource_key.get()
+		menu_name = 'see_all'
+		if resource.avatar:
+			query_params={'menu_name':menu_name,'resource_avatar':resource.avatar}
+			self.redirect('/?'+urllib.urlencode(query_params))				
+		else:
+			menu_name = 'see_your'
+			query_params={'menu_name':menu_name}
+			self.redirect('/?'+urllib.urlencode(query_params))				
+		
+		
 app = webapp2.WSGIApplication([
 	('/', MainPage),
 	('/sign', AddCreatedResource),
 	('/reserveResource', ReserveResource),
 	('/addReservation', AddReservation),
 	('/listTagResources', ListTagResources),
+	('/resourceRSS', RSSResource),
+	('/img', Image),
 ],debug=True)
